0e987b5d2208cdce8548442f6451bd0bc1fcbe0a
shreejanmdr <shreejan0497@gmail.com>
2023-08-09 21:11:51 +0545

    changed some UI and added more products

--- ShreejanEcommerce/src/main/resources/static/js/Indexscript.js
+++ ShreejanEcommerce/src/main/resources/static/js/Indexscript.js
@@ -17,11 +17,33 @@
 
 
 /*hamburger menu for compatible device */
-const bar=document.getElementById('bar');
-const nav=document.getElementById('navbar');
 
-if (bar){
+const bar = document.getElementById('bar');
+
+const close = document.getElementById('close');
+
+const nav = document.getElementById('navbar');
+
+
+
+if (bar) {
+
     bar.addEventListener('click', () => {
-        nav.classList.add('actv');
+
+        nav.classList.add('active');
+
     })
+
+}
+
+
+
+if (close) {
+
+    close.addEventListener('click', () => {
+
+        nav.classList.remove('active');
+
+    })
+
 }
--- ShreejanEcommerce/src/test/java/com/system/shreejanEcommerce/ProductRepositoryTest.java
+++ ShreejanEcommerce/src/test/java/com/system/shreejanEcommerce/ProductRepositoryTest.java
@@ -1,79 +1,81 @@
-//package com.system.shreejanEcommerce;
+package com.system.shreejanEcommerce;
+
+import com.system.shreejanEcommerce.entity.Products;
+import com.system.shreejanEcommerce.repo.ProductsRepo;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+import java.util.Optional;
+
+@DataJpaTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ProductRepositoryTest {
+    @Autowired
+    private ProductsRepo productsRepo;
+
+    @Test
+    @Order(1)
+    @Rollback(value = false)
+    public void saveProducts() {
+
+        Products products = Products.builder()
+                .name("TestName")
+                .quantity("1")
+                .price("1000")
+                .build();
 //
-//import com.system.shreejanEcommerce.entity.Products;
-//import com.system.shreejanEcommerce.repo.ProductsRepo;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.MethodOrderer;
-//import org.junit.jupiter.api.Order;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.TestMethodOrder;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.test.annotation.Rollback;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@DataJpaTest
-//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-//public class ProductRepositoryTest {
-//    @Autowired
-//    private ProductsRepo booksRepo;
-//
-//    @Test
-//    @Order(1)
-//    @Rollback(value = false)
-//    public void saveBooks() {
-//
-//        Products products = Products.builder()
-//                .name("TestName")
-//                .quantity("1")
-//                .price("1000")
-//                .build();
-////
-//        booksRepo.save(products);
-//        Assertions.assertThat(products.getId()).isGreaterThan(0);
-//
-//    }
-//
-//    @Test
-//    @Order(2)
-//    @Rollback(value = false)
-//    public void getBookTest() {
-//        Products products = booksRepo.findById(1).get();
-//        Assertions.assertThat(products.getId()).isEqualTo(1);
-//    }
-//
-//    //
-//    @Test
-//    @Order(3)
-//    public void fetchAll() {
-//        List<Products> products = booksRepo.findAll();
-//        Assertions.assertThat(products.size()).isGreaterThan(0);
-//    }
-//    //
-//    @Test
-//    @Order(4)
-//    @Rollback(value = false)
-//    public void Update() {
-//        Products products = booksRepo.findById(1).get();
-//        products.setName("TestName");
-//        Products books1 = booksRepo.save(products);
-//        Assertions.assertThat(books1.getName()).isEqualTo("TestName");
-//    }
-//    //
-//    @Test
-//    @Order(5)
-//    @Rollback(value = false)
-//    public void Delete(){
-//        Products products =booksRepo.findById(1).get();
-//        booksRepo.delete(products);
-//        Products books1=null;
-//        Optional<Products> bookOptional=booksRepo.findBybook_id(books1.getName());
-//        if(bookOptional.isPresent()){
-//            books1 = bookOptional.get();
-//        }
-//        Products books2=booksRepo.save(products);
-//        Assertions.assertThat(books2).isNull();
-//    }
-//}
+        productsRepo.save(products);
+        Assertions.assertThat(products.getId()).isGreaterThan(0);
+
+    }
+
+    @Test
+    @Order(2)
+    @Rollback(value = false)
+    public void getProductTest() {
+        Products products = productsRepo.findById(1).get();
+        Assertions.assertThat(products.getId()).isEqualTo(1);
+    }
+
+    //
+    @Test
+    @Order(3)
+    public void fetchAll() {
+        List<Products> products = productsRepo.findAll();
+        Assertions.assertThat(products.size()).isGreaterThan(0);
+    }
+    //
+    @Test
+    @Order(4)
+    @Rollback(value = false)
+    public void Update() {
+        Products products = productsRepo.findById(1).get();
+        products.setName("TestName");
+        Products products1 = productsRepo.save(products);
+        Assertions.assertThat(products1.getName()).isEqualTo("TestName");
+    }
+
+    @Test
+    @Order(5)
+    @Rollback(value = false)
+    public void Delete(){
+        Products products =productsRepo.findById(1).get();
+        productsRepo.delete(products);
+        Products products1=null;
+        Optional<Products> productOptional=productsRepo.findByproduct_id(products1.getName());
+        if(productOptional.isPresent()){
+            products1 = productOptional.get();
+        }
+        Products products2= productsRepo.save(products);
+        Assertions.assertThat(products2).isNull();
+
+    }
+
+}
